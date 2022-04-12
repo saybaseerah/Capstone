68841d9274e744e313cce8760287dbc7ac38db00
Baseerah <ogkbasirat@gmail.com>
2022-04-12 16:24:31 +0100

    Update bash arguments

--- Capstone_project.sh
+++ Capstone_project.sh
@@ -5,7 +5,7 @@ size=$1
 days=$2
 
 read -p "Please specify the directory containing your files: " directory
-echo -e "\nDeleting untouched files in $directory of $size modified $days ago"
+echo -e "\nFinding and deleting untouched files in $directory of $size modified $days ago"
 find $directory -type f -size $1 -mtime $2 -exec rm -f {} \;
 echo "Done"
 
@@ -16,6 +16,6 @@ echo "Enter the size of the files (in MB) to be found: "
 read size
 echo "Enter the days the files were last modified: "
 read days
-echo "Deleting untouched files in $name of $size modified $days ago"
+echo "Finding and deleting untouched files in $name of $size modified $days ago"
 find $name -type f -size $size -mtime $days -exec rm -f {} \;
 echo "Done"
