ef88496e2357205101d117ec1a0521fa30572275
Baseerah <ogkbasirat@gmail.com>
2022-04-12 14:15:35 +0100

    Update bash script

--- Capstone_project.sh
+++ Capstone_project.sh
@@ -4,20 +4,18 @@
 size=$1
 days=$2
 
-echo -e "\nUntouched files of $size modified less than $days ago"
-find Downloads1 -type f -size $1 -mtime $2
-echo -e "\nDeleting untouched files"
-find Downloads1 -type f -size $1 -mtime $2 -exec rm -f {} \;
+read -p "Please specify the directory containing your files: " directory
+echo -e "\nDeleting untouched files in $directory of $size modified $days ago"
+find $directory -type f -size $1 -mtime $2 -exec rm -f {} \;
 echo "Done"
 
 #Alternatively, this works also:
-echo "Enter the size of the files to be found: "
+
+read -p "Please specify the name of your directory: " name
+echo "Enter the size of the files (in MB) to be found: "
 read size
 echo "Enter the days the files were last modified: "
 read days
-echo "Untouched files of $size modified less than $days ago"
-find Downloads1 -type f -size $size -mtime $days
-echo "Deleting untouched files"
-find Downloads1 -type f -size $size -mtime $days -exec rm -f {} \;
+echo "Deleting untouched files in $name of $size modified $days ago"
+find $name -type f -size $size -mtime $days -exec rm -f {} \;
 echo "Done"
-
